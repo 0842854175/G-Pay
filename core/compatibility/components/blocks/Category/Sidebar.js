9a26de295be074f35c29bdbcbbbafa6332f9c1ff
RichShop <63937928+0842854175@users.noreply.github.com>
2020-08-02 10:36:38 +0700

    Initialize module_factory

---
+++ core/compatibility/components/blocks/Category/Sidebar.js
@@ -0,0 +1,42 @@
+import { buildFilterProductsQuery } from '@vue-storefront/core/helpers'
+import { mapGetters } from 'vuex'
+import pickBy from 'lodash-es/pickBy'
+
+export default {
+  name: 'CategorySidebar',
+  props: {
+    filters: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    ...mapGetters('category', ['getCurrentCategory', 'getActiveCategoryFilters', 'getCurrentCategoryProductQuery']),
+    category () {
+      return this.getCurrentCategory
+    },
+    activeFilters () {
+      return this.getActiveCategoryFilters
+    },
+    availableFilters () {
+      return pickBy(this.filters, (filter) => { return (filter.length) })
+    },
+    hasActiveFilters () {
+      return Object.keys(this.activeFilters).length !== 0
+    }
+  },
+  methods: {
+    sortById (filters) {
+      return [...filters].sort((a, b) => { return a.id - b.id })
+    },
+    resetAllFilters () {
+      this.$bus.$emit('filter-reset')
+      this.$store.dispatch('category/resetFilters')
+      this.$store.dispatch('category/searchProductQuery', {})
+      this.$store.dispatch('category/mergeSearchOptions', {
+        searchProductQuery: buildFilterProductsQuery(this.category, this.activeFilters)
+      })
+      this.$store.dispatch('category/products', this.getCurrentCategoryProductQuery)
+    }
+  }
+}
