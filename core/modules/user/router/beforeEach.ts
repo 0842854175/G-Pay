e09c9e82e0a6de1cc38b14f1ae36d0313f2efbfd
RichShop <63937928+0842854175@users.noreply.github.com>
2020-08-02 10:37:25 +0700

    Initialize serverless-test

---
+++ core/modules/user/router/beforeEach.ts
@@ -0,0 +1,23 @@
+import { Route } from 'vue-router'
+import rootStore from '@vue-storefront/core/store'
+import i18n from '@vue-storefront/i18n'
+import { isServer } from '@vue-storefront/core/helpers'
+
+export async function beforeEach(to: Route, from: Route, next) {
+  const requiresAuth = to.matched.some(route => route.meta.requiresAuth)
+  if (requiresAuth) {
+    if (isServer) {
+      next()
+    } else {
+      await rootStore.dispatch('user/startSession')
+      if (!rootStore.getters['user/isLoggedIn']) {
+        next('/')
+        localStorage.setItem('redirect', from.path)
+      } else {
+        next()
+      }
+    }
+  } else {
+    next()
+  }
+}
---
+++ src/themes/default/components/core/blocks/MyAccount/MyNewsletter.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="mb35">
+    <!-- My newsletter header -->
+    <div class="row mb15">
+      <div class="col-xs-12 col-sm-6">
+        <h3 class="m0 mb5">
+          {{ $t('My newsletter') }}
+        </h3>
+      </div>
+    </div>
+
+    <!-- My newsletter body (both modes) -->
+    <div class="row">
+      <div class="col-xs-12">
+        <h4>
+          {{ $t('General agreement') }}
+        </h4>
+      </div>
+
+      <base-checkbox
+        class="col-xs-12 mb25 cl-primary"
+        id="generalAgreement"
+        :value="user.isSubscribed"
+        @click="edit()"
+      >
+        {{ $t('I want to receive a newsletter, and agree to its terms') }}
+      </base-checkbox>
+
+      <div class="col-xs-12 col-sm-6">
+        <button-full @click.native="updateNewsletter">
+          {{ $t('Update my preferences') }}
+        </button-full>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import MyNewsletter from '@vue-storefront/core/compatibility/components/blocks/MyAccount/MyNewsletter'
+import ButtonFull from 'theme/components/theme/ButtonFull.vue'
+import BaseCheckbox from '../Form/BaseCheckbox.vue'
+
+export default {
+  components: {
+    ButtonFull,
+    BaseCheckbox
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+    edit () {
+      this.$set(this.user, 'isSubscribed', !this.user.isSubscribed)
+    }
+  },
+  mixins: [MyNewsletter]
+}
+</script>
