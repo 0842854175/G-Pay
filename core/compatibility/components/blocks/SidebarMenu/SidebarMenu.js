8b4d3d491988529b9192dcb21d06d9f4be695a43
RichShop <63937928+0842854175@users.noreply.github.com>
2020-08-02 10:35:46 +0700

    Initialize docs

---
+++ core/compatibility/components/blocks/SidebarMenu/SidebarMenu.js
@@ -0,0 +1,34 @@
+import { mapState } from 'vuex'
+import onEscapePress from '@vue-storefront/core/mixins/onEscapePress'
+import { CompareButton } from '@vue-storefront/core/modules/compare/components/CompareButton.ts'
+
+// depreciated as theme-specific
+export default {
+  name: 'SidebarMenu',
+  mixins: [onEscapePress, CompareButton],
+  computed: {
+    categories () {
+      return this.$store.state.category.list.filter((op) => {
+        return op.level === (this.$store.state.config.entities.category.categoriesDynamicPrefetchLevel ? this.$store.state.config.entities.category.categoriesDynamicPrefetchLevel : 2) // display only the root level (level =1 => Default Category), categoriesDynamicPrefetchLevel = 2 by default
+      })
+    },
+    ...mapState({
+      isOpen: state => state.ui.sidebar
+    }),
+    compareIsActive () {
+      // Computed property renamed to 'isEmpty'
+      return !this.isEmpty
+    }
+  },
+  created () {
+  },
+  methods: {
+    onEscapePress () {
+      this.closeMenu()
+    },
+    closeMenu () {
+      this.$store.commit('ui/setSidebar', false)
+      this.$store.commit('ui/setMicrocart', false)
+    }
+  }
+}
---
+++ src/themes/default/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <component :is="layout">
+    <router-view/>
+  </component>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+const DefaultLayout = () => import('./layouts/Default')
+const EmptyLayout = () => import('./layouts/Empty')
+const MinimalLayout = () => import('./layouts/Minimal')
+
+export default {
+  data () {
+    return {
+      ordersData: []
+    }
+  },
+  computed: {
+    ...mapState({
+      overlayActive: state => state.ui.overlay
+    }),
+    layout () {
+      return `${(this.$route.meta.layout || 'default')}-layout`
+    }
+  },
+  components: {
+    DefaultLayout,
+    EmptyLayout,
+    MinimalLayout
+  }
+}
+</script>
